69ac0dd18481b6d39ee206c468b59aad1afea89e
Ajay Narkhedkar <89686001+ajaynarkhedkar@users.noreply.github.com>
2025-10-01 22:11:50 +0530

    2nd commit

--- AgriShop.Common/Class1.cs
+++ AgriShop.Common/Class1.cs
@@ -4,7 +4,16 @@
     {
         public int Id { get; set; }
 
+        public int A { get; set; }
 
+
+
+
+
+
+
+
+        public int B { get; set; }
 
         public string Name { get; set; }
     }
