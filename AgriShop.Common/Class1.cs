d2b350af7ab191b072119413f81198dc0008451e
Ajay Narkhedkar <89686001+ajaynarkhedkar@users.noreply.github.com>
2025-10-02 11:14:13 +0530

    Update Class1.cs

--- AgriShop.Common/Class1.cs
+++ AgriShop.Common/Class1.cs
@@ -2,22 +2,6 @@
 {
     public class Class1
     {
-        public int Id { get; set; }
-
-        public int A
-        {
-            get; set;
-
-
-
-
-
-
-
-
-        public int B { get; set; }
-
-        public string Name { get; set; }
     }
 }
 
